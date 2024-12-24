13c7655410c0f12985f40546d7e755c4394d9776
ajaybhuj24 <bhujajay9@gmail.com>
2024-12-24 18:02:48 +0530

    Context API Created

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
-
+import { useNavigate } from "react-router-dom";
+import { AppContent } from "../context/AppContext";
 const Login = () => {
+  const navigate = useNavigate();
+  const { backendUrl, setIsLoggedin } = useContext(AppContent);
+
   const [state, setState] = useState("Sign Up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // const onSubmitHandler = async (e) => {
+  //   try {
+  //     e.preventDefault();
+  //     if (state === "Sign Up") {
+  //       await axios.po;
+  //     } else {
+  //     }
+  //   } catch (error) {}
+  // };
 
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
       <img
+        onClick={() => navigate("/")}
         src={assets.logo}
         alt="image"
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer "
       />
+
       <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
         <h2 className="text-3xl font-semibold text-white text-center mb-3 ">
           {state === "Sign Up" ? "create  account" : "Login "}
@@ -21,11 +40,13 @@ const Login = () => {
             : "Login to your account"}
         </p>
 
-        <form>
+        <form onSubmit={onSubmitHandler}>
           {state === "Sign Up" && (
             <div className=" mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
               <img src={assets.person_icon} alt="" />
               <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 className="bg-transparent outline-none "
                 type="text"
                 placeholder="Full Name"
@@ -37,28 +58,38 @@ const Login = () => {
           <div className=" mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.mail_icon} alt="" />
             <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="bg-transparent outline-none "
               type="email"
               placeholder="Enter Email"
               required
             />
           </div>
+
           <div className=" mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
             <img src={assets.lock_icon} alt="" />
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               className="bg-transparent outline-none "
               type="password"
               placeholder="Enter password"
               required
             />
           </div>
-          <p className="mb-4 text-indigo-500 cursor-pointer">
+
+          <p
+            onClick={() => navigate("/reset-password")}
+            className="mb-4 text-indigo-500 cursor-pointer"
+          >
             Forgot password?
           </p>
           <button className="w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900 text-white font-medium">
             {state}
           </button>
         </form>
+
         {state === "Sign Up" ? (
           <p className="text-gray-400 text-center text-xs mt-4">
             Already have an account?{" "}
