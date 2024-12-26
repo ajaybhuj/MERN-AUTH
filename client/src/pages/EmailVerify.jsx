09e18844724ecfc45ad5ec99fe91cc80923df3f3
ajaybhuj24 <bhujajay9@gmail.com>
2024-12-26 19:35:16 +0545

    email verification feature completed

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,9 +1,61 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { AppContent } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const EmailVerify = () => {
+  axios.defaults.withCredentials = true;
+  const { backendUrl, isLoggedIn, UserData, getUserData } =
+    useContext(AppContent);
+
   const navigate = useNavigate();
+
+  const inputRefs = React.useRef([]);
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const { otpArray } = inputRefs.current.map((e) => e.value);
+      const otp = otpArray.join("");
+      const { data } = await axios.post(
+        backendUrl + "/api/auth/verify-account",
+        { otp }
+      );
+      if (data.success) {
+        toast.success(data.message);
+        getUserData();
+        navigate("/");
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData("text");
+    const pasteArray = paste.split("");
+    pasteArray.array.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    });
+  };
   return (
     <div className="flex items-center justify-center min-h-screen  bg-gradient-to-br from-blue-200 to-purple-400">
       <img
@@ -12,18 +64,24 @@ const EmailVerify = () => {
         alt="image"
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer "
       />
-      <form className="bg-slate-900 p-8 rounded-lg w-96 text-sm">
+      <form
+        onSubmit={onSubmitHandler}
+        className="bg-slate-900 p-8 rounded-lg w-96 text-sm"
+      >
         <h1 className="text-white text-2xl font-semibold text-center mb-4">
           Email Verify OTP
         </h1>
         <p className="text-center mb-6 text-indigo-300">
           Enter the 6-digit code sent to your email
         </p>
-        <div className="flex justify-between mb-8">
+        <div className="flex justify-between mb-8 " onPaste={handlePaste}>
           {Array(6)
             .fill(0)
             .map((_, index) => (
               <input
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e.index)}
                 type="text"
                 maxLength="1"
                 key={index}
